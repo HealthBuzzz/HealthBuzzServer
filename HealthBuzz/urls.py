df1bd37962879f78b8eecacf82eda1e1ef7b2f77
sugsugsug <thddnrud2010@gmail.com>
2020-11-04 23:04:27 +0900

    csrf_exempt

--- HealthBuzz/urls.py
+++ HealthBuzz/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('buzz.urls'))
+    path('api/', csrf_exempt(include('buzz.urls')))
 ]
