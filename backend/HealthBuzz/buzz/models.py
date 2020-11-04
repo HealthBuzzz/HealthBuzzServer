e894a7e83f754c8e5c3c48e133398071c40dc11c
sugsugsug <thddnrud2010@gmail.com>
2020-11-04 18:59:04 +0900

    user view added

--- backend/HealthBuzz/buzz/models.py
+++ backend/HealthBuzz/buzz/models.py
@@ -1,28 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 
 # Create your models here.
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
 
 class StretchingData(models.Model):
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
     amount = models.IntegerField()
 
 class WaterData(models.Model):
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
     amount = models.IntegerField()
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance, user_pk=instance.id)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
--- backend/HealthBuzz/buzz/views.py
+++ backend/HealthBuzz/buzz/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import ensure_csrf_cookie
+from .models import StretchingData, WaterData
+import json
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        try:
+            req_data = json.loads(request.body.decode())
+            username = req_data['username']
+            email = req_data['email']
+            password = req_data['password']
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponse(status=400)
+        User.objects.create_user(username, email, password)
+        return HttpResponse(status=201)
+    else:
+        return HttpResponse(status=405)
+
+def signin(request):
+    if request.method == 'POST':
+        try:
+            req_data = json.loads(request.body.decode())
+            email = req_data['email']
+            password = req_data['password']
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponse(status=400)
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse(status=204)
+        else:
+            return HttpResponse(status=401)
+    else:
+        return HttpResponse(status=405)
+
+def signout(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return HttpResponse(status=204)
+        else:
+            return HttpResponse(status=401)
+    else:
+        return HttpResponse(status=405)
+
