73057de51939eaed63a7edfa39e3b5591d1c1294
KYHSGeekCode <jourhyang123@gmail.com>
2020-11-05 08:17:13 +0900

    :rocket: heroku postgres

--- HealthBuzz/settings.py
+++ HealthBuzz/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
