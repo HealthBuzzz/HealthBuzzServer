913278ea6c35e22242c8b1b32ea44b66ca348b34
KYHSGeekCode <jourhyang123@gmail.com>
2020-11-06 15:54:05 +0900

    :bug: Django staticfiles

--- HealthBuzz/settings.py
+++ HealthBuzz/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'HealthBuzz.urls'
@@ -133,5 +134,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # django_heroku.settings(locals())
