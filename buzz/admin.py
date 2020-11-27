d14209a1f56ed93aefa45f8b48fa39263aa30892
sugsugsug <thddnrud2010@gmail.com>
2020-11-27 17:13:09 +0900

    model add to see

--- buzz/admin.py
+++ buzz/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from buzz.models import Profile, DailyStretching, DailyWater
+
+admin.site.register(Profile)
+admin.site.register(DailyStretching)
+admin.site.register(DailyWater)
